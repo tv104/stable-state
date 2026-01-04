4fbd8017604ead80202f18146ae8c431e8dd17f4
tv104 <1164786+tv104@users.noreply.github.com>
2026-01-04 14:49:55 +0100

    increase white space below title/description

--- components/Container.tsx
+++ components/Container.tsx
@@ -7,7 +7,7 @@ interface ContainerProps {
 
 export function Container({ children, className = '' }: ContainerProps) {
     return (
-        <main className={`flex flex-1 flex-col items-center gap-6 px-4 py-12 max-w-6xl mx-auto w-full ${className}`}>
+        <main className={`flex flex-1 flex-col items-center gap-12 px-4 py-12 max-w-6xl mx-auto w-full ${className}`}>
             {children}
         </main>
     )
